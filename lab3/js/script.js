065f063178c01dc7ce19db0657b6cfd436b582fc
leodebuss <bionerd729@gmail.com>
2026-02-19 21:35:08 -0800

    lab3 final v1

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,6 +1,9 @@
 // Event listeners
 document.querySelector("button").addEventListener("click", gradeQuiz);
-
+document.addEventListener("DOMContentLoaded", function () {
+    let count = Number(localStorage.getItem("quizCount")) || 0;
+    document.getElementById("quizCount").textContent = count;
+});
 let score = 0;
 
 shuffleQ1Choices();
@@ -9,22 +12,20 @@ function shuffleQ1Choices() {
     q1Choices = _.shuffle(q1Choices);
     console.log(q1Choices);
 
-    // <label><input type="radio" name="q1" value="fontColor"> font-color</label>
-
     for (let i of q1Choices) {
-    let radioElement = document.createElement("input");
-    radioElement.type = "radio";
-    radioElement.name = "q1";
-    radioElement.value = i;
-    
-    let labelElement = document.createElement("label");
-    labelElement.textContent = i;
+        let radioElement = document.createElement("input");
+        radioElement.type = "radio";
+        radioElement.name = "q1";
+        radioElement.value = i;
 
-    labelElement.prepend(radioElement);
+        let labelElement = document.createElement("label");
+        labelElement.textContent = i;
 
-    document.querySelector("#q1ChoicesDiv").append(labelElement);
+        labelElement.prepend(radioElement);
 
-    console.log(radioElement);
+        document.querySelector("#q1ChoicesDiv").append(labelElement);
+
+        console.log(radioElement);
     }
 }
 
@@ -33,13 +34,14 @@ function gradeQuiz() {
     let answerQ1 = "Bad Bunny";
     let answerQ3 = "Jay-Z";
     let answerQ4 = 2000;
-    let answerQ5 = "Paul" && "Ringo";
+    let paulChecked = document.querySelector("#Paul").checked;
+    let ringoChecked = document.querySelector("#Ringo").checked;
+    let keithChecked = document.querySelector("#Keith").checked;
+    let mickChecked = document.querySelector("#Mick").checked;
     let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
-    let userAnswer3 = document.querySelector("#q3").value; 
+    let userAnswer3 = document.querySelector("#q3").value;
     let userAnswer4 = document.querySelector("#q4").value;
-    let userAnswer5 = document.querySelector("#Paul").value && document.querySelector("#Ringo").value; 
-
-    //alert("Grading quiz..." + userAnswer1 + " " + userAnswer3 + " " + userAnswer4);
+    let userAnswer5 = document.querySelector("#Paul").value && document.querySelector("#Ringo").value;
 
     if (userAnswer1 == answerQ1) {
         // display "Right!"
@@ -60,25 +62,29 @@ function gradeQuiz() {
         document.querySelector("#answer3").style.color = "green";
         score += 20;
     }
-     if (userAnswer4 == answerQ4) {
+    if (userAnswer4 == answerQ4) {
         // display "Right!"
         document.querySelector("#markImg4").innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
         document.querySelector("#answer4").textContent = "Correct";
         document.querySelector("#answer4").style.color = "green";
         score += 20;
     }
-    if (userAnswer5 == answerQ5) {
+    if (paulChecked && ringoChecked && !keithChecked && !mickChecked) {
         document.querySelector("#markImg5").innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
         document.querySelector("#answer5").textContent = "Correct";
         document.querySelector("#answer5").style.color = "green";
         score += 20;
+    } else {
+        document.querySelector("#markImg5").innerHTML ="<img src='img/xmark.jpg' alt='Incorrect'>";
+        document.querySelector("#answer5").textContent = "Incorrect";
+        document.querySelector("#answer5").style.color = "red";
     }
     if (userAnswer1 != answerQ1) {
         document.querySelector("#markImg1").innerHTML = "<img src='img/xmark.jpg' alt='xmark'>";
         document.querySelector("#answer").textContent = "Incorrect";
         document.querySelector("#answer").style.color = "red";
-    } 
-    if (userAnswer3!= answerQ3) {
+    }
+    if (userAnswer3 != answerQ3) {
         document.querySelector("#markImg3").innerHTML = "<img src='img/xmark.jpg' alt='xmark'>";
         document.querySelector("#answer3").textContent = "Incorrect";
         document.querySelector("#answer3").style.color = "red";
@@ -88,11 +94,6 @@ function gradeQuiz() {
         document.querySelector("#answer4").textContent = "Incorrect";
         document.querySelector("#answer4").style.color = "red";
     }
-    if (document.querySelector("#Keith").value || document.querySelector("#Mick").value) {
-        document.querySelector("#markImg5").innerHTML = "<img src='img/xmark.jpg' alt='xmark'>";
-        document.querySelector("#answer5").textContent = "Incorrect";
-        document.querySelector("#answer5").style.color = "red";
-    }
     document.querySelector("#totalScore").innerHTML = 'Total Score: ' + score;
     if (score >= 80) {
         document.querySelector("#message").textContent = "YAYYYY CONGRATS!!!";
@@ -101,4 +102,12 @@ function gradeQuiz() {
         document.querySelector("#message").textContent = "...oooooohh... listen to more music...";
         document.querySelector("#message").style.color = "red";
     }
+    quizCount();
+}
+
+function quizCount() {
+    let count = Number(localStorage.getItem("quizCount")) || 0;
+    count++;
+    localStorage.setItem("quizCount", count);
+    document.getElementById("quizCount").textContent = count;
 }
